120be192df1740ec19e1c178bdba4476f852e019
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-02-13 22:43:46 +0100

    Premenované "isDone" na "completed".

--- ToDoListVue/src/plugins/app/active-tasks/active-tasks.vue
+++ ToDoListVue/src/plugins/app/active-tasks/active-tasks.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <ul class="row justify-content-center list-group mt-3" v-if="areThereActiveTasks">
       <li class="list-group-item" v-for="task in filteredActiveTasks.slice().reverse()" :key="task.id">
-        <TaskItem v-if="!task.isDone" :task="task" @deleteTask="task.isDone = true"></TaskItem>
+        <TaskItem v-if="!task.completed" :task="task" @deleteTask="task.completed = true"></TaskItem>
       </li>
     </ul>
     <p class="text-center" v-else>Nie sú žiadne aktívne úlohy.</p>
@@ -19,10 +19,10 @@ export default {
   },
   computed: {
     areThereActiveTasks() {
-      return this.$store.state.tasks.filter((task) => !task.isDone) != 0 ? true : false;
+      return this.$store.state.tasks.filter((task) => !task.completed) != 0 ? true : false;
     },
     filteredActiveTasks() {
-      return this.$store.state.tasks.filter((task) => !task.isDone);
+      return this.$store.state.tasks.filter((task) => !task.completed);
     }
   }
 };
--- ToDoListVue/src/plugins/app/deleted-tasks/deleted-tasks.vue
+++ ToDoListVue/src/plugins/app/deleted-tasks/deleted-tasks.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <ul class="row justify-content-center list-group mt-3" v-if="areThereDeletedTasks">
       <li class="list-group-item" v-for="task in filteredDeletedTasks.slice().reverse()" :key="task.id">
-        <TaskItem v-if="task.isDone" :task="task" @deleteTask="deleteTask(task)"></TaskItem>
+        <TaskItem v-if="task.completed" :task="task" @deleteTask="deleteTask(task)"></TaskItem>
       </li>
     </ul>
     <p class="text-center" v-else>Nie sú žiadne odstránené úlohy.</p>
@@ -24,10 +24,10 @@ export default {
   },
   computed: {
     areThereDeletedTasks() {
-      return this.$store.state.tasks.filter((task) => task.isDone) != 0 ? true : false;
+      return this.$store.state.tasks.filter((task) => task.completed) != 0 ? true : false;
     },
     filteredDeletedTasks() {
-      return this.$store.state.tasks.filter((task) => task.isDone);
+      return this.$store.state.tasks.filter((task) => task.completed);
     }
   }
 };
