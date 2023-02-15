8ea9c54d5fe00c67adbe826e196f2d3de1430588
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2023-02-15 23:01:02 +0100

    Odstránený moxios plugin od Weze-a.

--- ToDoListVue/src/main.js
+++ ToDoListVue/src/main.js
@@ -3,14 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from './store';
 
-import moxios from '@/plugins/w/axios-mocks'
-
-moxios.mock({})
-moxios.mock({
-	'GET data': true,
-  'POST data': true,
-})
-
 const app = createApp(App)
   .use(store)
   .use(router)
--- ToDoListVue/src/plugins/w/axios-mocks/_REVIEW.md
+++
@@ -1,3 +0,0 @@
-
-# 25.1.
-[x] condition ci je moxios zapnuty by som vyhodnocoval cez !localStorage['isMoxios'], a nenastavoval by som tam 'false', ale unset, alebo prazdny string
