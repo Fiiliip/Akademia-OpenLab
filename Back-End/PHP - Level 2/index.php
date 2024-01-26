241fa8212f0ab17c8bbc55483464e1f2f60dd01e
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2024-01-26 15:33:10 +0100

    [PHP - Level 2] Added reading from input and query.

---
+++ Back-End/PHP - Level 2/index.php
@@ -0,0 +1,73 @@
+<?php
+    $currentDateTime = date("H:i:s d.m.Y", rand(0, time())); // Náhodný dátum a čas pre lepšiu simuláciu.
+?>
+
+<!DOCTYPE html>
+<html lang="sk-SK">
+<head>
+    <meta charset="utf-8">
+    <title>Logovač príchodov študentov</title>
+</head>
+<body>
+    <p>Aktuálny čas: <?php echo($currentDateTime); ?></p>
+    <form action='index.php' method='post'>
+        <input type='text' name='studentName' placeholder='Meno študenta'>
+        <input type='submit' value='Zapísať študenta'>
+    </form>
+</body>
+</html>
+
+<?php
+
+    class Student {
+        public $name;
+        public $arrivalCounter;
+    }
+
+    if (isset($_GET['studentName']) && $_GET['studentName'] != "")
+    {
+        $studentName = $_GET['studentName'];
+        $studentArrivalTime = $currentDateTime;
+    } 
+    else if (isset($_POST['studentName']) && $_POST['studentName'] != "")
+    {
+        $studentName = $_POST['studentName'];
+        $studentArrivalTime = $currentDateTime;
+    }
+
+    $student = new Student();
+    $student->name = $studentName;
+    $student->arrivalCounter = 1;
+    
+    writeToJSON($student);
+
+    /////////////////////////////////////////
+
+    function writeToJSON($content)
+    {
+        $studentExists = false;
+        if (file_exists('studenti.json'))
+        {
+            $content = file_get_contents('studenti.json');
+            $content = json_decode($content);
+
+            for ($i = 0; $i < count($content); $i++)
+            {
+                if ($content[$i]->name == $content->name)
+                {
+                    $studentExists = true;
+                    $content[$i]->arrivalCounter += 1;
+                    break;
+                }
+            }
+        }
+
+        if (!$studentExists)
+        {
+            array_push($content, $content);
+        }
+
+        file_put_contents('studenti.json', $content);
+    }
+
+?>
