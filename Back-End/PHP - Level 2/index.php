e4fc736f8afa6a95a88e5f1af1f5af90a46cda36
Fiiliip <50334258+Fiiliip@users.noreply.github.com>
2024-01-30 10:34:41 +0100

    [PHP - Level 2] Added Student class.

--- Back-End/PHP - Level 2/index.php
+++ Back-End/PHP - Level 2/index.php
@@ -18,56 +18,85 @@
 </html>
 
 <?php
+include('Student.php');
 
-    class Student {
-        public $name;
-        public $arrivalCounter;
-    }
-
-    if (isset($_GET['studentName']) && $_GET['studentName'] != "")
+    if (isset($_GET['studentName']) && $_GET['studentName'] != "") 
     {
         $studentName = $_GET['studentName'];
         $studentArrivalTime = $currentDateTime;
-    } 
-    else if (isset($_POST['studentName']) && $_POST['studentName'] != "")
+    } else if (isset($_POST['studentName']) && $_POST['studentName'] != "")
     {
         $studentName = $_POST['studentName'];
         $studentArrivalTime = $currentDateTime;
-    }
-
-    $student = new Student();
-    $student->name = $studentName;
-    $student->arrivalCounter = 1;
-    
-    writeToJSON($student);
-
-    /////////////////////////////////////////
-
-    function writeToJSON($content)
+    } else 
     {
-        $studentExists = false;
-        if (file_exists('studenti.json'))
-        {
-            $content = file_get_contents('studenti.json');
-            $content = json_decode($content);
-
-            for ($i = 0; $i < count($content); $i++)
-            {
-                if ($content[$i]->name == $content->name)
-                {
-                    $studentExists = true;
-                    $content[$i]->arrivalCounter += 1;
-                    break;
-                }
-            }
-        }
-
-        if (!$studentExists)
-        {
-            array_push($content, $content);
-        }
-
-        file_put_contents('studenti.json', $content);
+        die();
     }
+
+    $students = Student::getStudentsFromJSON();
+
+    foreach ($students as $student)
+    {
+        if ($student['name'] == $studentName) {
+
+        }
+    }
+
+?>
+
+<?php
+
+    // class Student {
+    //     public $name;
+    //     public $arrivalCounter;
+    // }
+
+    // if (isset($_GET['studentName']) && $_GET['studentName'] != "")
+    // {
+    //     $studentName = $_GET['studentName'];
+    //     $studentArrivalTime = $currentDateTime;
+    // } 
+    // else if (isset($_POST['studentName']) && $_POST['studentName'] != "")
+    // {
+    //     $studentName = $_POST['studentName'];
+    //     $studentArrivalTime = $currentDateTime;
+    // }
+
+    // $student = new Student();
+    // $student->name = $studentName;
+    // $student->arrivalCounter = 1;
+    
+    // writeToJSON($student);
+
+    // /////////////////////////////////////////
+
+    // function writeToJSON($content)
+    // {
+    //     $studentExists = false;
+    //     if (file_exists('studenti.json'))
+    //     {
+    //         $content = file_get_contents('studenti.json');
+    //         $content = json_decode($content);
+
+    //         print_r($content);
+
+    //         for ($i = 0; $i < count($content); $i++)
+    //         {
+    //             if ($content[$i]->name == $content->name)
+    //             {
+    //                 $studentExists = true;
+    //                 $content[$i]->arrivalCounter += 1;
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     if (!$studentExists)
+    //     {
+    //         array_push($content, $content);
+    //     }
+
+    //     file_put_contents('studenti.json', $content);
+    // }
 
 ?>
